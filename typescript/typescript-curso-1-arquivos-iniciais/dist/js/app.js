98bc33045a1d153a27e504416f8a421d6bad91d5
israel-bsi <masterisraelsilva@gmail.com>
2025-08-12 17:11:56 -0300

    Adicionado projeto de typescript

---
+++ typescript/typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -0,0 +1,6 @@
+import { Negociacao } from "./models/negociacao.js";
+const negociacao = new Negociacao(new Date(), 10, 1000);
+console.log(negociacao.data);
+console.log(negociacao.quantidade);
+console.log(negociacao.valor);
+console.log(negociacao.volume);
